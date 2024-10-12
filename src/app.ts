d66c45a96f67601410170cbbf1fac8ec8029fa53
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2024-10-12 16:41:57 +0600

    project setup complete

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
-const app: Application = express()
-const PORT = process.env.PORT || 5000
-app.use(express.json())
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
 app.get('/', async (req: Request, res: Response) => {
-  res.send('Kheye dibo kintu!')
-})
+  res.send('Kheye dibo kintu!');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
